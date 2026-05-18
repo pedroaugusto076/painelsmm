a464a2714013615adf0754ac2f1e820f18e6b834
pedroaugusto076 <userpedro555@gmail.com>
2026-05-18 16:39:34 -0300

    fix: corrigir CORS para funcoes serverless

--- api/payments/balance.js
+++ api/payments/balance.js
@@ -4,13 +4,15 @@ const mercadopago = require('mercadopago');
 const crypto = require('crypto');
 
 module.exports = async function handler(req, res) {
+  // CORS headers
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
 
   if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+    res.status(200).end();
+    return;
   }
 
   try {
